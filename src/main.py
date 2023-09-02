4d6bad56055d5824ecb0888eff37001ae65be4fd
RajKKapadia <raajforyou@gmail.com>
2023-09-02 14:08:29 +0530

    added fulfilment

--- src/main.py
+++ src/main.py
@@ -1,7 +1,33 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def handle_home():
     return 'OK', 200
+
+
+@app.route('/fulfilment', methods=['POST'])
+def handle_fulfilment():
+    body = request.get_json()
+    print(body)
+    '''TODO
+    (1) get user information
+    (2) store the user information
+    (3) create a respone
+    (4) send the response
+    '''
+    return jsonify(
+        {
+            "fulfillmentMessages": [
+                {
+                    "text": {
+                        "text": [
+                            "From my own server."
+                        ]
+                    }
+                }
+            ]
+        }
+    )
